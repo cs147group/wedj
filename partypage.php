c582311fa50a9d2ace1458f93c153886ff211cd0
Leslie <leslierogers@rescomp-11-124344.Stanford.EDU>
2011-10-27 22:05:50 -0700

    adding html to party

--- partypage.php
+++ partypage.php
@@ -1,1 +1,28 @@
-there will be a PARTYPAGE here
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta name="viewport" content="user-scalable=no,width=640px"/>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type"  />
+<link href="wedj.CSS" rel="stylesheet" type="text/css" />
+<title>We DJ!</title>
+</head>
+
+<body>
+
+<div id="header">
+     <div id="back-arrow" class="inline">
+     	  <a href="playlist.php"><img src="back.png"/></a>
+	  </div>
+	  <div id="logo" class="inline">
+	       <br />
+	       	   WE DJ</div>
+</div>
+<div id="main">
+     <p>
+	PARTYTIME
+	</p>
+	</div>
+</body>
+
+</html>

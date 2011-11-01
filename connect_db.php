344cbc2eb8f4985a23b5f9a4bccbbed11632fb24
Leslie <leslierogers@DN52212a.SUNet>
2011-11-01 11:16:32 -0700

    more work on databases

--- connect_db.php
+++ connect_db.php
@@ -1,10 +1,9 @@
 <?php
-	$conn = mysql_connect("mysql.cs147.org", "jpulvera", "eymQq6V");
-	if (!$conn){
-	   echo("could not connect:(");
-	}
-	if(!mysql_select_db("jpulvera_mysql"){
-		echo("could not find database");
-	}	
-
+	$dbHost = "mysql.cs147.org";
+	$dbUser = "jpulvera";
+	#dbPass = "eymQqu6V";
+	$dbDatabase = "jpulvera_mysql";
+	$db = mysql_connect("$dbHost", "$dbUser", "$dbPass") or die ("Error connecting to database");
+	$db_found = mysql_select_db("$dbDatabase", $db) or die ("Couldn't select the database");
+	
 ?>

8fadf0d8d7181c1288ba8fb53fa3d0cdb15618d5
Bill Driscoll <bill.driscoll22@stanford.edu>
2011-11-04 01:43:32 -0700

    hi

--- updatePlaylist.php
+++ updatePlaylist.php
@@ -24,12 +24,20 @@
 			<h3><?php echo $row["name"];?></h3>
 			<p><?php echo $row["artist"];?></p>
 			<span class="ui-li-count"><?php echo $currRating;  ?></span>
-			<div data-role="controlgroup" data-type="horizontal">
-			<?php 
-				echo '<a data-icon="arrow-u" data-iconpos="notext" data-role="button" href="#" class="like-button" id ="' . $currSongID . '"></a>';
-				echo '<a data-icon="arrow-d" data-iconpos="notext" data-role="button" href="#" class="dislike-button" id ="' . $currSongID . '"></a>';
-			?>
-				</div>
+			<div data-role="controlgroup" data-type="horizontal" class="ui-corner-all ui-controlgroup ui-controlgroup-horizontal">
+  <a data-icon="arrow-u" data-iconpos="notext" data-role="button" href="#" class="like-button ui-btn ui-btn-icon-notext ui-corner-left ui-btn-up-c" id="<?php echo $currSongID; ?>" title="" data-theme="c">
+    <span class="ui-btn-inner ui-corner-left" aria-hidden="true">
+      <span class="ui-btn-text"></span>
+      <span class="ui-icon ui-icon-arrow-u ui-icon-shadow"></span>
+    </span>
+  </a>
+  <a data-icon="arrow-d" data-iconpos="notext" data-role="button" href="#" class="dislike-button ui-btn ui-btn-icon-notext ui-corner-right ui-controlgroup-last ui-btn-up-c" id="<?php echo $currSongID; ?>" title="" data-theme="c">
+    <span class="ui-btn-inner ui-corner-right ui-controlgroup-last" aria-hidden="true">
+      <span class="ui-btn-text"></span>
+      <span class="ui-icon ui-icon-arrow-d ui-icon-shadow"></span>
+    </span>
+  </a>
+</div>
 			<?php
                 }
     		?></li>

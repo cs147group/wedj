816fd63c358c00352c3161c744f4606aab6bed4f
Leslie <ljrogers27@gmail.com>
2011-11-08 14:45:00 -0800

    limit users to one vote each

--- updatePlaylist.php
+++ updatePlaylist.php
@@ -2,10 +2,46 @@
 	include("connect_db.php");
 	$id = $_POST["songID"];
 	$isUp = $_POST["isUp"];
-	if($isUp == 1) $result = mysql_query("UPDATE playlist SET rating = rating + 1 WHERE songID = $id");
-	else $result = mysql_query("UPDATE playlist SET rating = rating - 1 WHERE songID = $id");
-	
 	$ip = $_SERVER['REMOTE_ADDR'];
+
+
+
+        $alreadyVotedQuery = "SELECT * FROM votes WHERE ip = '$ip' AND songID = $id";
+        $votedResult = mysql_query($alreadyVotedQuery) or die(mysql_error());
+
+        $numVoteResults = mysql_num_rows($votedResult);
+        if ($numVoteResults == 0){      //if they've never voted on this song before, they can vote either way
+           if($isUp == 1) {
+	   	    $result = mysql_query("UPDATE playlist SET rating = rating + 1 WHERE songID = $id");
+         	    	    $insertVoteQuery = "INSERT INTO votes (ip, songID, isUpvote) VALUES ('$ip', '$id', '1')";
+         		    		     mysql_query($insertVoteQuery) or die(mysql_error());
+					     } else {
+					       $result = mysql_query("UPDATE playlist SET rating = rating - 1 WHERE songID = $id");
+         				       	       $insertVoteQuery = "INSERT INTO votes (ip, songID, isUpvote) VALUES ('$ip', '$id', '0')";
+         					       			mysql_query($insertVoteQuery) or die(mysql_error());
+									}
+        } else {                         //if they've already voted once
+
+          $voteRow = mysql_fetch_array($votedResult) or die(mysql_error());
+          $prevVote = $voteRow['isUpvote'];
+          if($isUp == 0){
+              if($prevVote == 1){ //They already upvoted once but are now changing to a downvote
+                      echo "you already upvoted, but are changing to a downvote";
+                      $result = mysql_query("UPDATE playlist SET rating = rating - 2 WHERE songID = $id");
+                      $updateVoteQuery = "UPDATE votes SET isUpvote = '0' WHERE ip = '$ip' AND songID = '$id' AND isUpvote = '1' LIMIT 1";
+                      mysql_query($updateVoteQuery) or die(mysql_error());
+              } else echo "you already downvoted and want to downvote again.  LAME";
+          } else if ($prevVote == 0) { //They already downvoted but are changing to an upvote
+                 echo "you already downvoted, but now you're tryna upvote";
+                 $result = mysql_query("UPDATE playlist SET rating = rating + 2 WHERE songID = $id");
+                 $updateVoteQuery = "UPDATE votes SET isUpvote = '1' WHERE ip = '$ip' AND songID = '$id' AND isUpvote = '0'  LIMIT 1";
+                 mysql_query($updateVoteQuery) or die(mysql_error());
+          } else echo "you already upvoted and want to upvote again NUH UH";
+        }
+
+
+
+
 	$query = "SELECT party FROM users WHERE ip='$ip';";
 	$result = mysql_query($query) or die(mysql_error());
   if ($row = mysql_fetch_array($result)) {

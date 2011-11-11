3efa70d6a114d7775f74662010c696e45170f80f
Leslie <ljrogers27@gmail.com>
2011-11-10 19:01:08 -0800

    prevents mysql errors when not in a party

--- information-gatherer.php
+++ information-gatherer.php
@@ -1,8 +1,14 @@
 <?php
 	$ipAddress = $_SERVER['REMOTE_ADDR'];
-
 	$userQuery = "SELECT * FROM users WHERE ip='$ipAddress'";
 	$userResult = mysql_query($userQuery) or die(mysql_error());
+	if(mysql_affected_rows() == 0) {
+		?>
+		<script type = "text/javascript">
+			window.location = "index.php";
+		</script>
+		<?php
+	}
 	$isHost = mysql_result($userResult, 0, "isAdmin");
 	$partyID = mysql_result($userResult, 0, "party");
 

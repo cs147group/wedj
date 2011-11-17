5cc3ed9f09ba57a29f36857523c31bfb88e01b75
Bill Driscoll <bill.driscoll22@stanford.edu>
2011-11-16 18:49:16 -0800

    for john

--- _vti_cnf/index.php
+++ _vti_cnf/index.php
@@ -1,9 +1,13 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|15 Nov 2011 23:11:35 -0000
+vti_timelastmodified:TR|17 Nov 2011 02:42:23 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|bill-PC\\bill
 vti_modifiedby:SR|bill-PC\\bill
 vti_timecreated:TR|15 Nov 2011 23:11:35 -0000
+vti_backlinkinfo:VX|index.php information-gatherer.php
+vti_syncofs_kolyyu22.cs147.org\:22/kolyyu22.cs147.org/wedj6:TW|15 Nov 2011 23:14:45 -0000
+vti_syncwith_kolyyu22.cs147.org\:22/kolyyu22.cs147.org/wedj6:TX|15 Nov 2011 23:11:35 -0000
+vti_nexttolasttimemodified:TW|15 Nov 2011 23:11:35 -0000
 vti_cacheddtm:TX|15 Nov 2011 23:11:35 -0000
 vti_filesize:IR|1957
 vti_cachedbodystyle:SR|<body>
@@ -14,6 +18,3 @@ vti_cachedhasbots:BR|true
 vti_cachedhastheme:BR|false
 vti_cachedhasborder:BR|false
 vti_charset:SR|utf-8
-vti_backlinkinfo:VX|index.php information-gatherer.php
-vti_syncofs_kolyyu22.cs147.org\:22/kolyyu22.cs147.org/wedj6:TW|15 Nov 2011 23:14:45 -0000
-vti_syncwith_kolyyu22.cs147.org\:22/kolyyu22.cs147.org/wedj6:TX|15 Nov 2011 23:11:35 -0000
--- _vti_cnf/updateNowPlaying.php
+++ _vti_cnf/updateNowPlaying.php
@@ -1,11 +1,11 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|bill-PC\\bill
 vti_modifiedby:SR|bill-PC\\bill
-vti_timelastmodified:TR|17 Nov 2011 01:05:10 -0000
+vti_timelastmodified:TR|17 Nov 2011 02:24:29 -0000
 vti_timecreated:TR|16 Nov 2011 22:41:28 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|
-vti_nexttolasttimemodified:TW|17 Nov 2011 01:00:35 -0000
+vti_nexttolasttimemodified:TW|17 Nov 2011 01:05:10 -0000
 vti_syncofs_kolyyu22.cs147.org\:22/kolyyu22.cs147.org/wedj6:TW|17 Nov 2011 01:05:28 -0000
 vti_syncwith_kolyyu22.cs147.org\:22/kolyyu22.cs147.org/wedj6:TW|17 Nov 2011 01:05:10 -0000
 vti_cacheddtm:TX|17 Nov 2011 01:05:10 -0000
